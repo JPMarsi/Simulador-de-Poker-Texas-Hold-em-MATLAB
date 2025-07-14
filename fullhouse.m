398abb0c95b3f0ca38f10e64373f7262b51ec968
JPMarsi <jpmarsigliani@gmail.com>
2025-07-14 12:13:47 -0300

    Delete fullhouse.m

--- fullhouse.m
+++
@@ -1,6 +0,0 @@
-% Función auxiliar para verificar Full
-function es_full=fullhouse(valores)
-    unicos=unique(valores);
-    repeticiones=histc(valores, unicos);
-    es_full=any(repeticiones==3) && any(repeticiones==2);
-end
