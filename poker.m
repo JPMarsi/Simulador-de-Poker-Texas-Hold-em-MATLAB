3036c329ae97c5a7ddd9bc9d6243a9b6aeba5319
JPMarsi <jpmarsigliani@gmail.com>
2025-07-14 12:15:01 -0300

    Delete poker.m

--- poker.m
+++
@@ -1,6 +0,0 @@
-% Función auxiliar para verificar Poker
-function es_poker=poker(valores)
-    unicos=unique(valores);
-    repeticiones=histc(valores, unicos);
-    es_poker=any(repeticiones==4);
-end
