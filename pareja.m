b1c63bc1b8dad2ae0effbe127c59c52afbfab817
JPMarsi <jpmarsigliani@gmail.com>
2025-07-14 12:14:53 -0300

    Delete pareja.m

--- pareja.m
+++
@@ -1,6 +0,0 @@
-% Función auxiliar para verificar Pareja
-function es_pareja=pareja(valores)
-    unicos=unique(valores);
-    repeticiones=histc(valores, unicos);
-    es_pareja=any(repeticiones==2);
-end
