b1cbdcdcdd9a9a4613b052f9d0ccadb252d0bf21
JPMarsi <jpmarsigliani@gmail.com>
2025-07-14 12:13:11 -0300

    Delete doblepareja.m

--- doblepareja.m
+++
@@ -1,6 +0,0 @@
-% Función auxiliar para verificar Doble Pareja
-function es_doble_pareja=doblepareja(valores)
-    unicos=unique(valores);
-    repeticiones=histc(valores, unicos);
-    es_doble_pareja=sum(repeticiones == 2)==2;
-end
